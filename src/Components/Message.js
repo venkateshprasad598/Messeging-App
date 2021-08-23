b756c080270adbfc5e9c6b4addb7a26b22083ac0
venkateshprasad598 <venkateshprasad598@gmail.com>
2021-08-23 13:08:03 +0530

    add

--- src/Components/Message.js
+++ src/Components/Message.js
@@ -3,11 +3,13 @@ import { Typography, Card, CardContent } from "@material-ui/core";
 import "./Message.css";
 
 const Message = ({ userName, message }) => {
-  const isUser = userName === message.userName;
+  const isUser = userName === message.userName; // isUser = true
   return (
-    <div>
+    <div className={`message ${isUser && "message__user"}`}>
+      {/*Wrapping up the card in a div and aligning it either left or a right, based on a isUser condition*/}
       {message.text ? (
-        <Card className={`message ${isUser && "message__user"}`}>
+        <Card className={isUser ? "message__userCard" : "message__guestCard"}>
+          {/*Performing action on a card */}
           <CardContent>
             <Typography color="white" variant="h5" component="h2">
               {message.userName}: {message.text}
